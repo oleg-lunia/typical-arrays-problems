54d12ac941afcd1fb150620efbc0fde6df3aea7b
Oleg Lunia <oleglunja2@gmail.com>
2021-03-10 00:13:40 +0300

    refactoring: changed the file index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
-
 exports.min = function min (array) {
-  return 0;
-}
-
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+    if (array === undefined) {
+        return 0;
+    }
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] >= 0 || array[i] <= 0) {
+            return Math.min.apply(null, array);
+        }else {
+            return 0;
+        }
+    }
+    if (array = []) {
+        return 0;
+    }
+  }
+  
+  exports.max = function max (array) {
+    if (array === undefined) {
+        return 0;
+    }
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] >= 0 || array[i] <= 0) {
+            return Math.max.apply(null, array);
+        }else {
+            return 0;
+        }
+    }
+    if (array = []) {
+        return 0;
+    }
+  }
+  
+  exports.avg = function avg (array) {
+    if (array === undefined) {
+        return 0;
+    }
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] >= 0 || array[i] <= 0) {
+            let result = array.reduce(function(sum, current) {
+                return sum + current
+              });
+            return (result / array.length);
+        }else {
+            return 0;
+        }
+    }
+    if (array = []) {
+        return 0;
+    }
+  }
